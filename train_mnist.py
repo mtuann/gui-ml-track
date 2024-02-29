e67c34f30950aae9231f6d7d57c9114f23762a06
Tuan Nguyen <mtuann4@gmail.com>
2024-02-29 10:36:42 +0700

    add log training

--- train_mnist.py
+++ train_mnist.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
+import sys
+import time
+import tqdm
+
 
 # Define the neural network architecture
 class CNN(nn.Module):
@@ -33,44 +37,105 @@ transform = transforms.Compose([
 train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1024, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1024, shuffle=False)
 
 # Instantiate the CNN model
 model = CNN()
 
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Training loop
-num_epochs = 10
-for epoch in range(num_epochs):
-    # Training phase
-    model.train()
-    running_loss = 0.0
-    for i, (inputs, labels) in enumerate(train_loader):
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        if (i+1) % 100 == 0:
-            print(f'Training - Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {running_loss/100}')
-            running_loss = 0.0
+hyper_params = {
+    "optimizer": "adam",
+    "learning_rate": 0.001,
+    "num_epochs": 5
+}
+
+
+def get_training_data(train_params):
     
-    # Testing phase
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for inputs, labels in test_loader:
+    lr = train_params["learning_rate"]
+    optimizer = train_params["optimizer"]
+    num_epochs = train_params["num_epochs"]
+    
+    if optimizer == "adam":
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+    else:
+        optimizer = optim.SGD(model.parameters(), lr=lr)
+    
+    meta_data = {
+        "train_loss": [],
+        "train_accuracy": [],
+        "test_loss": [],
+        "test_accuracy": [],
+        "time_training": [],
+    }
+    
+    # Training loop
+    start_time = time.time()
+    
+    print('Start Training')
+    
+    for epoch in range(num_epochs):
+        # Training phase
+        model.train()
+        training_loss, training_correct, train_total = 0.0, 0.0, 0
+        tqdm.tqdm.write(f'Epoch [{epoch+1}/{num_epochs}]')
+        tqdm_loader = tqdm.tqdm(enumerate(train_loader), total=len(train_loader))
+        for i, (inputs, labels) in tqdm_loader:
+            optimizer.zero_grad()
             outputs = model(inputs)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = 100 * correct / total
-    print(f'Testing - Epoch [{epoch+1}/{num_epochs}], Accuracy: {accuracy}%')
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            training_loss += loss.item()
+            num_correct = (torch.argmax(outputs, dim=1) == labels).sum().item()
+            training_correct += num_correct
+            train_total += len(labels)
+            
+            # if (i+1) % 100 == 0:
+            # tqdm.tqdm.write(f'Training - Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {training_loss/100} Accuracy: {training_correct*100/train_total}%')
+            # update progress bar
+            tqdm_loader.set_description(f'Training - Epoch [{epoch+1}/{num_epochs}], Loss: {training_loss/100:.4f} Accuracy: {training_correct*100/train_total:.4f}%')
+            
+            #     print(f'Training - Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {running_loss/100}')
+            #     running_loss = 0.0
+        train_accuracy = 100 * training_correct / train_total
+        print(f'Training - Epoch [{epoch+1}/{num_epochs}], Accuracy: {train_accuracy:.4f}%')
+        # Testing phase
+        model.eval()
+        test_loss, test_correct, test_total = 0.0, 0.0, 0
+        
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs, 1)
+                test_total += labels.size(0)
+                test_correct += (predicted == labels).sum().item()
+        test_accuracy = 100 * test_correct / test_total
+        print(f'Testing - Epoch [{epoch+1}/{num_epochs}], Accuracy: {test_accuracy:.4f}%')
 
-print('Finished Training')
+        elapsed_time = time.time() - start_time
+        meta_data["train_loss"].append(training_loss)
+        meta_data["train_accuracy"].append(train_accuracy)
+        meta_data["time_training"].append(elapsed_time)
+        meta_data["test_loss"].append(test_loss)
+        meta_data["test_accuracy"].append(test_accuracy)
+        
+    print('Finished Training')
+    return meta_data
+
+if __name__ == "__main__":
+    # get parameters from command line
+    args = sys.argv
+    hyper_params["optimizer"] = args[1]
+    hyper_params["learning_rate"] = float(args[2])
+    hyper_params["num_epochs"] = int(args[3])
+    # sample command: python train_mnist.py adam 0.001 5
+    meta_data = get_training_data(hyper_params)
+    # dump meta_data to a json file with indent=2
+    import json
+    with open('meta_data.json', 'w') as f:
+        json.dump(meta_data, f, indent=2)
+    print('meta_data.json has been saved')
